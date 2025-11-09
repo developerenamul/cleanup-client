0de07a0a490faeb09e322e7d0bf87804d639b705
developerenamul <developerenamul@gmail.com>
2025-11-10 03:12:56 +0600

    client site setup complete

---
+++ cleanuphub-client/src/firebase/firebase.int.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCObuEJBJe__3dbGzd6czk7SsD2OBypX0w",
+  authDomain: "cleanup-auth.firebaseapp.com",
+  projectId: "cleanup-auth",
+  storageBucket: "cleanup-auth.firebasestorage.app",
+  messagingSenderId: "418601757115",
+  appId: "1:418601757115:web:eca9ddd15d3e186641fc9e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
